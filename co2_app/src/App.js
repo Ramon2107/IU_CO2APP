91a97a67426352e7a6f0f7f2d28749d86eca60ce
Ramon2107 <ramon.esteves-salgueiro@iu-study.org>
2025-06-09 22:07:13 +0200

    Erste lauffähige Version

--- co2_app/src/App.js
+++ co2_app/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 
 function App() {
-  return (
-      <div className="App">
-        <Header />
-        <Content />
-        <Footer />
-      </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Content />
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
-
